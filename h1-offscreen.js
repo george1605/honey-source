a59ec9139890fe80f1926cf6ebc0b9c8f63ec304
george1605 <32997165+george1605@users.noreply.github.com>
2025-01-01 17:52:05 +0200

    Nicified h1-offscreen

--- h1-offscreen.js
+++ h1-offscreen.js
@@ -1,1 +1,20 @@
-chrome.runtime.onMessage.addListener((function(e){if("offscreen:tag"===e.type){var t=document.createElement("iframe");t.style.display="none",t.sandbox="allow-forms allow-same-origin allow-scripts",t.src=e.affUrl,t.id="myFrame",document.body.appendChild(t),setTimeout((function(){t.remove()}),9e4);var a=document.getElementById("myFrame");chrome.runtime.sendMessage({type:"offscreen:tag:success",msg:"tagged wtih ".concat(e.affUrl," ").concat(e.storeId," ").concat(a.id)})}}));
+chrome.runtime.onMessage.addListener(function(config) {
+  if ("offscreen:tag" === config.type) {
+    
+    var iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    iframe.sandbox = "allow-forms allow-same-origin allow-scripts";
+    iframe.src = config.affUrl;
+    iframe.id = "myFrame";
+    document.body.appendChild(iframe);
+    setTimeout(function() {
+      iframe.remove();
+    }, 90000);
+
+    var that = document.getElementById("myFrame");
+    chrome.runtime.sendMessage({
+      type : "offscreen:tag:success",
+      msg : "tagged wtih ".concat(config.affUrl, " ").concat(config.storeId, " ").concat(that.id)
+    });
+  }
+});
